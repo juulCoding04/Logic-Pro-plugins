e3271df1d3ed48a5c293e09e92df0c59bf7d63ae
Juul <juul20018@gmail.com>
2026-09-07 15:24:28 +0200

    changing first-plugin GUI to have a volume slider

--- first-plugin/Source/PluginEditor.cpp
+++ first-plugin/Source/PluginEditor.cpp
@@ -1,15 +1,32 @@
 #include "PluginEditor.h"
+#include "juce_graphics/juce_graphics.h"
+#include "juce_gui_basics/juce_gui_basics.h"
 
 HelloWorldPluginAudioProcessorEditor::HelloWorldPluginAudioProcessorEditor (HelloWorldPluginAudioProcessor& p)
     : AudioProcessorEditor (&p), processorRef (p)
 {
-    setSize (400, 300);
+    setSize (200, 200);
+
+    midiVolume.setSliderStyle(juce::Slider::LinearBarVertical);
+    midiVolume.setRange (0.0, 127.0, 1.0);
+    midiVolume.setTextBoxStyle (juce::Slider::NoTextBox, false, 90, 0);
+    midiVolume.setPopupDisplayEnabled (true, false, this);
+    midiVolume.setTextValueSuffix (" Volume");
+    midiVolume.setValue (1.0);
+
+    // adds slider to the editor
+    addAndMakeVisible (&midiVolume);
 }
 
 void HelloWorldPluginAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-    g.setColour (juce::Colours::white);
-    g.setFont (30.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.fillAll (juce::Colours::white);
+    g.setColour (juce::Colours::black);
+    g.setFont (15.0f);
+    g.drawFittedText ("Midi Volume", 0, 0, getWidth(), 30, juce::Justification::centred, 1);
+}
+
+void HelloWorldPluginAudioProcessorEditor::resized() {
+    // sets the position and size of the slider with arguments (x, y, width, height)
+    midiVolume.setBounds (40, 30, 20, getHeight() - 60);
 }
--- first-plugin/Source/PluginEditor.h
+++ first-plugin/Source/PluginEditor.h
@@ -1,17 +1,21 @@
 #pragma once
 #include "PluginProcessor.h"
+#include "juce_gui_basics/juce_gui_basics.h"
 
 class HelloWorldPluginAudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
-    explicit HelloWorldPluginAudioProcessorEditor (HelloWorldPluginAudioProcessor&);
+    HelloWorldPluginAudioProcessorEditor (HelloWorldPluginAudioProcessor&);
     ~HelloWorldPluginAudioProcessorEditor() override = default;
 
     void paint (juce::Graphics&) override;
-    void resized() override {}
+    void resized() override;
 
 private:
     HelloWorldPluginAudioProcessor& processorRef;
+
+    // create a slider object to controll midi volume
+    juce::Slider midiVolume;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (HelloWorldPluginAudioProcessorEditor)
 };
